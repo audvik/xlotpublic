fccf40db5251936a960b9d552fabbd441944f56a
Manish Sahu <manishnfriends@gmail.com>
2025-05-12 21:21:44 +0545

    dist

---
+++ dist/assets/input-error-DvIj_zfl.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-DN9aw3Pt.js";import{c as o}from"./button-CAfXsGlv.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
